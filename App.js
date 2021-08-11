85ff4443e61cfb44b5b7a4404d4a75c8e5ea3030
Alyna Palamarchuk <alynapchuk@gmail.com>
2021-08-11 12:51:08 -0500

    added login and user auth

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 
 import firebase from 'firebase';
 
@@ -15,20 +15,72 @@ firebase.initializeApp({
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text } from 'react-native';
 
 import Landing from './components/auth/Landing';
 import Register from './components/auth/Register';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    }
+  }
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Landing'>
-        <Stack.Screen name='Otter' component={Landing} />
-        <Stack.Screen name='Register' component={Register} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  // DISPLAY LOADING SCREEN WHILE STATE IS LOADED
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      // IF NOT LOADED OR LOGGED IN
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+        // USER IS LOGGED IN
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+
+  render() {
+
+    const { loaded, loggedIn } = this.state;
+
+    // IF STATE NOT LOADED, DISPLAYS LOADING SCREEN
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+
+    // IF USER NOT LOGGED IN, DISPLAY LOGIN/REGISTER
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Landing'>
+            <Stack.Screen name='Otter' component={Landing} />
+            <Stack.Screen name='Register' component={Register} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    }
+
+    // IF USER IS LOGGED IN, DISPLAYS HOME PAGE
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>Placeholder</Text>
+      </View>
+    )
+  }
 }
+
+export default App

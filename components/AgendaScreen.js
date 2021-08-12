70ab9c288030c9d7be475fff87417aab998a8f97
otis wilcox <otiswilcox@gmail.com>
2021-08-12 16:54:50 -0400

    my attempt at agenda screen

---
+++ components/AgendaScreen.js
@@ -0,0 +1,59 @@
+import { React, useState } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { Agenda } from "react-native-calendars";
+
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split("T")[0];
+};
+
+const AgendaScreen = () => {
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity>
+        <View>
+          <Text>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={"2017-05-16"}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+};
+
+export default AgendaScreen;

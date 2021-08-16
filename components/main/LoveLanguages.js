b1c01032fe05f0a47dc0bb25e67400fd5045ac61
emhhd7 <eharpermarie@gmail.com>
2021-08-16 16:35:36 -0400

    accessed quiz and questions information, button is functional

--- components/main/LoveLanguages.js
+++ components/main/LoveLanguages.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native';
+import { Languages, Questions } from '../quiz/Quiz'
 
 export class LoveLanguages extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+          loaded: false,
+          text: "state was here",
+          currentPage: "start"
+        };
+    }
 
-    render() {
+    start() {
         return (
             <View style={styles.container}>
                 <Text>Love Language Quiz</Text>
-                <Button title="Start" onPress={() => console.log("Buttton Tapped.")}></Button>
+                <Button title="Start" onPress={() => this.setQuestions()}></Button>
             </View>
+        )
+    }  
+    
+    setQuestions(){
+        this.setState({
+            currentPage: "questions"
+        })
+    }
+
+    questions(){
+        return (
+            <View style={styles.container}>
+                <Text>It's more meaningful to me when...</Text>
+                <Text>{Questions[0][0].text}</Text>
+                <Text>{Questions[0][1].text}</Text>
+            </View>
+        )
+    }
+    render() {
+        const currentPage = {
+            "start": this.start(),
+            "questions": this.questions()
+        }
+        return (
+            currentPage[this.state.currentPage]
         )
     }
 }

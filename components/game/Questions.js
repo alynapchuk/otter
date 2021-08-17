2dfe7e680b132ddc2c6bb53b381ec8971dbeea03
Joshua Simon <ga.simonj@gmail.com>
2021-08-17 17:06:03 -0400

    added Questions.js to components file

---
+++ components/game/Questions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+export default function Questions(){
+    return(
+<Text>
+    <ul>
+        <li>Have you ever broken up with someone via text?</li>
+        <li>Have you ever dated a friend's ex?</li>
+        <li>Have you ever lied for a good reason?</li>
+        <li>Have you ever held in something you wish you would have shared?</li>
+    </ul>
+</Text>
+    )
+}
